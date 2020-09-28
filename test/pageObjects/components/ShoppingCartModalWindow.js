d3a90ec25fc4b34244848a1c4595468fc858b75b
Alexander Kiyko <avkiyko@gmail.com>
2020-09-27 23:08:20 -0400

    add ShoppingCartModalWindow.js PO

---
+++ test/pageObjects/components/ShoppingCartModalWindow.js
@@ -0,0 +1,27 @@
+class ShoppingCartModalWindow {
+  get continueShoppingBtn() {
+    return $('//span[contains(@class, "continue")]');
+  }
+
+  get ProceedToCheckoutBtn() {
+    return $('//a[@title="Proceed to checkout"]')
+  }
+
+  get productAddedText() {
+    return $('//div[contains(@class, "layer_cart_product")]//h2');
+  }
+
+  get productsCount() {
+    return $('//div[contains(@class, "layer_cart_cart")]//h2');
+  }
+
+  get productNameText() {
+    return $('//span[@id="layer_cart_product_title"]');
+  }
+
+  get closeWindowBtn() {
+    return $('//span[@title="Close window"]');
+  }
+}
+
+export default new ShoppingCartModalWindow();
