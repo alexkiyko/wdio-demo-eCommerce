ae86b10120d487f02be380fcbcb0c8960ac0636e
Alexander Kiyko <avkiyko@gmail.com>
2020-10-17 00:03:48 -0400

    add tc

--- test/specs/smoke/singleProductAddToCart.spec.js
+++ test/specs/smoke/singleProductAddToCart.spec.js
@@ -4,32 +4,33 @@ import Header from './../../pageObjects/components/Header';
 import Products from './../../pageObjects/Products.page';
 import ShoppingCartModalWindow from "../../pageObjects/components/ShoppingCartModalWindow";
 import Utils from "../../helpers/utils";
+import productsPageData from './../../data/productsPage.data';
 
 describe('Single Product Add To Cart', () => {
   before('Open Home Page', () => {
     HomePage.open();
   });
 
-  before('Go to Women Products and add to cart first product', () => {
-    Utils.click(Header.menuWomenProductsBtn);
-    Utils.click(Products.viewListBtn);
-    Utils.click(Products.addToCartBtn(0));  // add to cart first product from list with index 0
-    expect(Utils.getText(Products.headerText)).equal('WOMEN ');
+  it('Go to Women Products - Verify Women Products page loaded', () => {
+    Header.goToWomenProducts();
+    expect(Utils.getText(Products.title)).equal(productsPageData.title.women);
   });
 
-  let productName;
+  let firstProductName;
+
+  it('Add to cart first product - Verify Cart Modal Window loaded', function () {
+    Products.viewProductsList();
+    Utils.click(Products.addToCartBtn(0));  // add to cart first product from list with index 0
+    expect(Utils.isDisplayed(ShoppingCartModalWindow.cartModalWindow)).to.be.true;
+  });
 
   it('Verify correct product added to cart by product name', () => {
-    productName = Products.productName(0).getText(); // text of the first product in the list of products with index 0
-    expect(Utils.getText(ShoppingCartModalWindow.productNameText)).equal(productName);
-  });
-
-  it('Verify success message displayed', () => {
-    expect(Utils.getText(ShoppingCartModalWindow.productAddedText))
-      .equal('Product successfully added to your shopping cart');
+    firstProductName = Products.productName(0).getText(); // text of the first product in the list of products with index 0
+    expect(Utils.getText(ShoppingCartModalWindow.productNameText)).equal(firstProductName);
   });
 
   it('Verify Cart contains 1 product',  () => {
-    expect(Utils.getText(ShoppingCartModalWindow.productsCount).replace(/[^0-9]/g, '')).equal('1');
+    expect(Utils.getText(ShoppingCartModalWindow.productsCount).replace(/[^0-9]/g, ''))
+      .equal('1');
   });
 });
